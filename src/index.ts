dcb31ab79f862a11d84a22e685774e9fc47d5369
Morio Chiba <47474097+oriom-dev@users.noreply.github.com>
2026-09-25 18:13:29 +0900

    feat: forward emails

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,9 @@ type AttachmentAccumulator = {
 export default {
 	async email(message: ForwardableEmailMessage, env: Env, ctx: ExecutionContext): Promise<void> {
 		try {
+			// メールの転送
+			message.forward(env.FORWARD_TO_EMAIL)
+
 			// メールの解析
 			const parser = new PostalMime();
 			const rawEmail = await new Response(message.raw).arrayBuffer();
